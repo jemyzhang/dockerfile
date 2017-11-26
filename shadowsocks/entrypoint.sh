e43e06adcc887d7651b659db013cb5711eeefde1
jemyzhang <jemy.zhang@gmail.com>
2017-11-26 21:28:54 +0800

    add udp2raw

--- shadowsocks/entrypoint.sh
+++ shadowsocks/entrypoint.sh
@@ -5,8 +5,12 @@ SS_MODULE=${SS_MODULE:-"ss-server"}
 KCP_CONFIG=${KCP_CONFIG:-""}
 KCP_MODULE=${KCP_MODULE:-"kcpserver"}
 KCP_FLAG=${KCP_FLAG:-"false"}
+U2R_CONFIG=${U2R_CONFIG:-""}
+U2R_MODULE=${U2R_MODULE:-"udp2raw"}
+U2R_FLAG=${U2R_FLAG:-"false"}
 
-while getopts "s:m:k:e:x" OPT; do
+
+while getopts "s:m:k:e:x:u:v:w" OPT; do
     case $OPT in
         s)
             SS_CONFIG=$OPTARG;;
@@ -18,8 +22,22 @@ while getopts "s:m:k:e:x" OPT; do
             KCP_MODULE=$OPTARG;;
         x)
             KCP_FLAG="true";;
+        u)
+            U2R_CONFIG=$OPTARG;;
+        v)
+            U2R_MODULE=$OPTARG;;
+        w)
+            U2R_FLAG="true";;
     esac
 done
+
+if [ "$U2R_FLAG" == "true" ] && [ "$U2R_CONFIG" != "" ]; then
+    echo -e "\033[32mStarting udp2raw......\033[0m"
+    $U2R_MODULE $U2R_CONFIG --gen-add 2>&1
+    $U2R_MODULE $U2R_CONFIG 2>&1 &
+else
+    echo -e "\033[33mudp2raw not started......\033[0m"
+fi
 
 if [ "$KCP_FLAG" == "true" ] && [ "$KCP_CONFIG" != "" ]; then
     echo -e "\033[32mStarting kcptun......\033[0m"
